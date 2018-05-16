7252f66476f6a699959bb2720d87cf2649015847
kokorink <kokorin.konstantin@gmail.com>
2018-05-17 03:06:38 +0700

    API

---
+++ WebApplication1/API/QuestionsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DomainModel;
+
+namespace WebApplication1.API
+{
+    public class QuestionsController : ApiController
+    {
+        // GET: api/Questions
+        public IEnumerable<Questions> Get()
+        {
+            IGenericRepository<Questions> RepQ = new GenericRepository<Questions>();
+            List<Questions> ListQ = RepQ.GetAll().ToList();
+            return ListQ;
+
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Questions/5
+        public Questions Get(int id)
+        {
+            IGenericRepository<Questions> RepQ = new GenericRepository<Questions>();
+            Questions question = RepQ.FindBy(item => item.Id == id).FirstOrDefault();
+            return question;
+        }
+
+        // POST: api/Questions
+        public void Post([FromBody]Questions quetstion)
+        {
+            if (quetstion.Question != null || quetstion.UserMail != null || quetstion.UserName != null)
+            {
+                IGenericRepository<Questions> RepQ = new GenericRepository<Questions>();
+                quetstion.TimeQuestion = DateTime.Now;
+                RepQ.Add(quetstion);
+                RepQ.Save();
+
+            }
+    
+        }
+
+        // PUT: api/Questions/5
+        public void Put(int id, [FromBody]Questions quetstion)
+        {
+            if (quetstion != null || quetstion.UserMail != null || quetstion.UserName != null)
+            {
+                IGenericRepository<Questions> RepQ = new GenericRepository<Questions>();
+                RepQ.Edit(quetstion);
+                RepQ.Save();
+            } 
+        }
+
+        // DELETE: api/Questions/5
+        public void Delete(int id)
+        {
+            IGenericRepository<Questions> RepQ = new GenericRepository<Questions>();
+            Questions quetstion = RepQ.FindBy(item => item.Id == id).FirstOrDefault();
+            RepQ.Delete(quetstion);
+            RepQ.Save();
+        }
+    }
+}
--- WebApplication1/App_Start/WebApiConfig.cs
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebApplication1
@@ -13,6 +14,9 @@ namespace WebApplication1
 
             // Маршруты веб-API
             config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+               .Add(new MediaTypeHeaderValue("text/html"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
